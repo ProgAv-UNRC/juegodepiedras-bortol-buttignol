08c83a2fd8e3a2491891ba91ab01ffd71068c9c3
Juan Pablo Bortol <51797823+jpbn456@users.noreply.github.com>
2023-06-14 11:44:54 -0300

    Correct indentation of Piedras.hs

--- Piedras.hs
+++ Piedras.hs
@@ -1,26 +1,22 @@
 -- | Este es el codigo fuente del TP para la materia Progamación Avanza - 2023 - UNRC
 -- El texto tiene varias funciones y tipos de datos ya implementados, el trabajo consiste en completar aquellas funciones 
 -- que no se encuentran implementadas (es decir, estan definidas con 'undefined').  
-module Piedras where
+
+module Piedras where 
 
 -- | El siguiente tipo define los jugadores del juego, C por Computadora
 --   y H por Humano
-data Jugador
-  = C
-  | H
-  deriving (Eq, Show)
+data Jugador = C | H deriving (Eq,Show)
+
 
 -- | Definimos los estados, un estado es un jugador mas las cantidad de piedras disponibles
-type Estado = (Jugador, Int)
+type Estado = (Jugador, Int) 
 
 -- | Definimos los posibles estados del juegos, el resultado del juego, puede ser que la computadora pierda, o gane
-data Resultado
-  = CPerdio
-  | CGano
-  deriving (Eq, Ord, Show)
+data Resultado = CPerdio | CGano deriving (Eq,Ord,Show) 
 
 -- | Definimos las posible jugadas, sacar 1 piedra, 3 piedras o 4 piedras
-jugadas = [1, 3, 4]
+jugadas = [1,3,4]
 
 -- | La funcion otro Jugador, dado un jugador, devuelve el otro jugador, por ejemplo: otroJugador C = H
 otroJugador :: Jugador -> Jugador
@@ -29,32 +25,19 @@ otroJugador H = C
 
 -- | Dada una jugada (cantidad de piedras que se retiran) y un estado retorna el estado resultante, se deben controlar los casos de jugadas no posibles
 hacerJugada :: Int -> Estado -> Estado
-hacerJugada n (j, k)
-  | n > k =
-    error
-      ("Jugada no valida. Se quisieron quitar" ++
-       (show n) ++
-       "piedras, pero solo hay" ++ (show k) ++ "piedras disponibles.")
-  | n `elem` jugadas =
-    error
-      ("Jugada no valida. Se quisieron quitar" ++
-       (show n) ++ "piedras, pero solo se pueden quitar 1, 3 o 4 piedras.")
-  | otherwise = (otroJugador j, k - n)
+hacerJugada n (j, k)    | n > k = error ("Jugada no valida. Se quisieron quitar" ++ (show n) ++ "piedras, pero solo hay" ++ (show k) ++ "piedras disponibles.")
+                        | n `elem` jugadas = error ("Jugada no valida. Se quisieron quitar" ++ (show n) ++ "piedras, pero solo se pueden quitar 1, 3 o 4 piedras.")
+                        | otherwise = (otroJugador j, k - n)
 
 -- |  evalEstado toma un estado como parametro, y dice si el estado es ganador o perdedor considerando
 --  las mejores jugadas del oponente. Por ejemplo, evalEstado (H,2) = CGano, porque H solo puede
 --  retirar 1 y luego la computadora retira 1 y gana
 evalEstado :: Estado -> Resultado
-evalEstado (j, k)
-  | (k == 0) =
-    if j == C
-      then CPerdio
-      else CGano
-  | k > 0 && j == C = foldl max CPerdio $ map evalEstado posibleJugs
-  | k > 0 && j == H = foldl min CGano $ map evalEstado posibleJugs
-  | otherwise = error "jugada no valida"
-  where
-    posibleJugs = [(otroJugador j, k - i) | i <- jugadas, i <= k]
+evalEstado (j, k)   | (k == 0) = if j == C then CPerdio else CGano
+                    |  k>0 && j == C = foldl max CPerdio $ map evalEstado posibleJugs
+                    |  k>0 && j == H = foldl min CGano $ map evalEstado posibleJugs   
+                    |  otherwise = error "jugada no valida"
+                    where posibleJugs = [(otroJugador j, k - i) | i<- jugadas, i<=k]    
 
 -- | Calcula la mejor jugada para un estado dado, para el jugador dado.
 -- Por ejemplo, mejorJug (H,3) debería devolver 3, ya que la mejor jugada para H cuando hay 3 piedras es retirar 3.
@@ -67,47 +50,37 @@ evalEstado (j, k)
 -- 	En el caso mejorJug (H, k) tenemos que devolver la jugada que nos da el valor minimo (es decir, consideramos 
 -- 	la mejor jugada para H, que seria la peor para C).
 mejorJug :: Estado -> Int
-mejorJug (j, k)
-  | j == H =
-    if (length jugadasH) > 0
-      then maximum jugadasH
-      else 4
-  | j == C =
-    if (length jugadasC) > 0
-      then minimum jugadasC
-      else 4
-  | otherwise = error "jugada no valida"
-  where
-    jugadasH =
-      [i | i <- posibleJugs, evalEstado (otroJugador j, k - i) == CPerdio]
-    jugadasC =
-      [i | i <- posibleJugs, evalEstado (otroJugador j, k - i) == CGano]
-    posibleJugs = [i | i <- jugadas, i <= k]
+mejorJug (j, k) | j == H = if (length jugadasH) > 0 then maximum jugadasH else error ("No hay una mejor jugada posible para el estado " ++ (show (j,k)))
+                | j == C = if (length jugadasC) > 0 then minimum jugadasC else error ("No hay una mejor jugada posible para el estado " ++ (show (j,k)))
+                | otherwise = error "jugada no valida"
+                where 
+                    jugadasH = [i | i <- posibleJugs, evalEstado (otroJugador j, k-i) == CPerdio]
+                    jugadasC = [i | i <- posibleJugs, evalEstado (otroJugador j, k-i) == CGano]
+                    posibleJugs = [i | i <- jugadas, i <= k]
+
 
 -- | Las siguientes funciones implementan una pequeña interface para poder jugar interactivamente,.
-jugar :: Estado -> IO ()
-jugar (j, k) = do
-  putStrLn ("Hay " ++ (show k) ++ " piedras, cuantas saca?:")
-  jugada <- getLine
-  let s = read jugada
-      (j', k') = hacerJugada s (j, k)
-  if k' == 0
-    then (putStrLn "Gano!")
-    else do
-      let mj = mejorJug (j', k')
-      putStrLn ("mi jugada: " ++ (show mj))
-      if k' - mj == 0
-        then putStrLn "Perdio!"
-        else do
-          jugar (H, k' - mj)
+jugar :: Estado -> IO()
+jugar (j,k) = do
+            putStrLn ("Hay "++ (show k) ++ " piedras, cuantas saca?:")
+            jugada <-  getLine
+            let s  = read jugada
+                (j', k') = hacerJugada s (j,k)
+            if k'==0 then (putStrLn "Gano!")
+            else do
+                let mj = mejorJug (j',k')
+                putStrLn ("mi jugada: "++(show mj))
+                if k' - mj == 0
+                then putStrLn "Perdio!"
+                else do
+                    jugar (H, k' - mj)
 
 -- | Comienza el juego con una cantidad de piedras dada el Humano  
-comenzarJuego :: Int -> IO ()
-comenzarJuego cant
-  | cant <= 0 = error "La cantidad de piedras debe ser mayor que 0."
-  | otherwise = jugar (H, cant)
+comenzarJuego :: Int -> IO()
+comenzarJuego cant  | cant <= 0 = error "La cantidad de piedras debe ser mayor que 0."
+                    | otherwise = jugar (H, cant)
 
 -- juegosGanadores k, calcula todos los comienzos ganadores para la computadora hasta con k piedras
 -- por ejemplo, juegosGanadores 10 = [2,7,9]
 juegosGanadores :: Int -> [Int]
-juegosGanadores i = [x | x <- [1 .. i], evalEstado (H, x) == CGano]
+juegosGanadores i = [x | x <- [1..i], evalEstado (H, x) == CGano]
